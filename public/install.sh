0a24cf92c643906540203aa643246726618788be
ubermanu <manu@paca.happy-dev.fr>
2023-11-05 16:21:51 +0100

    add install script

---
+++ public/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPT_URL="https://raw.githubusercontent.com/ubermanu/sudoas/main/sudo.bash"
+DESTINATION_PATH="$HOME/.local/bin/sudo"
+
+# Check if curl is installed
+if ! command -v curl &> /dev/null; then
+  echo "Error: 'curl' is not installed. Please install 'curl' and try again."
+  exit 1
+fi
+
+if curl -o "$DESTINATION_PATH" "$SCRIPT_URL"; then
+  chmod +x "$DESTINATION_PATH"
+  echo "sudo script installed in $DESTINATION_PATH"
+else
+  echo "Failed to download and install the sudoas script."
+fi
